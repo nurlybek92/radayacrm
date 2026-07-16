d6f6e5909ec81bf6288ebe77c477c081c061c0b5
NZ <nurlybek.zakariya@gmail.com>
2026-07-16 18:17:44 +0500

    Fix menu bug on login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -26,8 +26,7 @@ export default function LoginPage() {
       const data = await res.json();
 
       if (res.ok && data.success) {
-        router.refresh();
-        router.push('/');
+        window.location.href = '/';
       } else {
         setError(data.error || 'Ошибка авторизации. Попробуйте еще раз.');
       }
